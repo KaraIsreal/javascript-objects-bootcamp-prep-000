95c97d78e1d26611988caf0ec2a040a0f58b2bd9
Kara Isreal <isreal.kara92@gmail.com>
2018-09-07 16:44:43 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,9 @@
+var playlist = {
+  fredHammond: "Song of Strength",
+  brianCourtneyWilson: "Noise",
+  unitedTenors: "Here In Our Praise",
+}; 
+
+function updatePlaylist(playlist, 'maryMary: Yesterday'){
+return Object.assign({}, playlist, 'maryMary: Yesterday')
+}
