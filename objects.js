52406e56e00c0136f7c0f07c1861f664ebb56af4
Kara Isreal <isreal.kara92@gmail.com>
2018-09-07 17:10:42 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,5 +2,5 @@ var playlist = { artist: "song" };
 
 function updatePlaylist(playlist, 'artist: song') {
   playlist.artist = ["song", "song2"];
-   return Object.assign ({}, playlist, {artist: ['song', 'song2']});
+ Object.assign ({}, playlist, {artist: ['song', 'song2']});
 }
